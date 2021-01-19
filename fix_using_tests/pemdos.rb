595616b9dd064ff33355a6f90cd5b37a41bb305a
Jede Brahma <brahmajede@gmail.com>
2021-01-19 05:01:39 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,9 @@
-# don't forget to add: require 'pry'
-
+require 'pry'
 def snake_it_up(string)
-  if string[0] == "s"
+  if "string".slice(0) == "s"
     10 * "s" + string
   else
     string
+    binding.pry
   end
 end
