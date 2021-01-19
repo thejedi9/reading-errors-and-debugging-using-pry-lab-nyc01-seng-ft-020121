a8d826144966b7dc916732a09981ee88d723c9c6
Jede Brahma <brahmajede@gmail.com>
2021-01-19 05:14:04 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,9 @@
-# don't forget to add: require 'pry'
 
-def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
+def snake_it_up (string)
+  if string.slice(0) == "s"
+    "s" *10 + string
   else
     string
+
   end
 end
